adb41af8585082c78cc413bef5e2796d9fa58736
Luis Morales <luismc2211mail.com>
2022-11-10 17:38:53 -0600

    Fix Login

--- ulsafoods_cliente/src/Componentes/Login/Login.js
+++ ulsafoods_cliente/src/Componentes/Login/Login.js
@@ -1,3 +1,7 @@
+import logo from '../images/logo2.png';
+import burger from '../images/burger.jpg';
+
+
 function Login() {
     const inicio = () => {
         window.location.href = '/Inicio';
@@ -5,10 +9,10 @@ function Login() {
     return (
         <div className="CuadroLogin">
             <div className="Izquierda">
-                <a href="/#"><img src="https://i.imgur.com/eFWRUuR.jpg" alt="Logo" /></a>
+                <a href="/#"><img src={burger} alt="Logo" /></a>
             </div>
             <div className="Derecha">
-                <a href="/#"><img src="https://cdn-icons-png.flaticon.com/512/98/98017.png" alt="Logo" /></a>
+                <a href="/#"><img src={logo} alt="Logo" /></a>
                 <input type="text" id="user" className="input font" placeholder="Usuario" />
                 <input type="password" className="input font" name="password" id="password" placeholder="Contraseña" required />
                 <button className="Entrar" type="submit" onClick={inicio}>
