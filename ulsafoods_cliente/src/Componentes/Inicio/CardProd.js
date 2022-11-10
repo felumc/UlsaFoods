49467d1c7921cd29ca2a68ff7833e34329ddaf45
EduardoVarela0144 <014426634@ulsaoaxaca.edu.mx>
2022-11-10 14:07:18 -0600

    Tarjetas de productos

---
+++ ulsafoods_cliente/src/Componentes/Inicio/CardProd.js
@@ -0,0 +1,50 @@
+import '../../Css/Inicio/CardProd.css'
+
+
+function CardProd() {
+   
+
+    return (
+        <div className="row">
+            <div className="col-sm-6 col-md-6 col-lg-4">
+                <div className="food-card">
+                    <div className="food-card_img">
+                        <img src="https://i.imgur.com/eFWRUuR.jpg" alt="" />
+                    </div>
+                    <div className="food-card_content">
+                        <div className="food-card_title-section">
+                            <p className="food-card_title">Nombre de producto</p>
+                            <p className="food-card_author">Categoria</p>
+                        </div>
+                        <div className="food-card_bottom-section">
+                            <div className="space-between">
+                                <div>
+                                    <button className="Agregar">Agregar a carrito</button>
+                                </div>
+                            </div>
+                            <hr />
+                            <div className="space-between">
+                                <div className="food-card_price">
+                                    <span>5.99$</span>
+                                </div>
+                                <div className="food-card_order-count">
+                                    <div className="input-group mb-3">
+                                        <div  className="input-group-prepend">
+                                            <button   className="btn btn-outline-secondary minus-btn" type="button" id="button-addon1"><i className="fa fa-minus"></i></button>
+                                        </div>
+                                        <input type="text" className="form-control input-manulator" placeholder="" aria-label="Example text with button addon" aria-describedby="button-addon1" />
+                                        <div className="input-group-append">
+                                            <button  className="btn btn-outline-secondary add-btn" type="button" id="button-addon1"><i className="fa fa-plus"></i></button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    
+    )
+}
+export { CardProd }
--- ulsafoods_cliente/src/Paginas/Inicio.js
+++ ulsafoods_cliente/src/Paginas/Inicio.js
@@ -1,8 +1,8 @@
 import { NavBar } from '../Componentes/Inicio/NavBar';
 import {ProdNav} from '../Componentes/Inicio/ProdNav';
+import {CardProd} from '../Componentes/Inicio/CardProd';
 import { Container } from '../Componentes/General/Container';
 import '../Css/Inicio/Inicio.css'
-
 function Inicio() {
     return (
         <>
@@ -10,6 +10,8 @@ function Inicio() {
             <h2>Página de inicio</h2>
             <Container>
             <ProdNav/>
+            <br/>
+            <CardProd/>
             </Container>
         </>
     )
